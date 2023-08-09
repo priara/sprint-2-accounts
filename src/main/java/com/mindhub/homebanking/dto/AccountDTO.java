3aa071bbb16f3fe62245c471a6134ff78c4913db
priara <priscilaiss16@hotmail.com>
2023-08-09 17:37:06 -0300

    first commit sprint 3

--- src/main/java/com/mindhub/homebanking/dto/AccountDTO.java
+++ src/main/java/com/mindhub/homebanking/dto/AccountDTO.java
@@ -19,6 +19,7 @@ public class AccountDTO {
     private double balance;
 
     private List<TransactionDTO> transaction;
+   /* hola*/
     public AccountDTO() {
     }
     public AccountDTO(Account account){
